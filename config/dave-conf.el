66984edef3451ffc1f95175c4840e34f844f2218
David Maldonado <david.m.maldonado@gmail.com>
2013-12-17 13:01:26 -0500

    backing up current settings - planning on complete revamp

--- config/dave-conf.el
+++ config/dave-conf.el
@@ -3,9 +3,9 @@
 ;; C-n newlines if at end of buffer
 (setq next-line-add-newlines t)
 
-;; line numbering
-(global-linum-mode 1)
-(setq linum-format "%d ")
+;; line numbering (off until I figure out weird yellow block glitch)
+;;(global-linum-mode 1)
+;;(setq linum-format "  %d ")
 
 ;; STUFFS FOR ALL LANGUAGES
 
@@ -21,7 +21,9 @@
 ;; autopair
 (add-to-list 'load-path "~/.live-packs/dave-pack/lib/autopair")
 (require 'autopair)
-(autopair-global-mode)
+(defvar autopair-modes '(ruby-mode java-mode c++-mode c-mode haskell-mode))
+(defun turn-on-autopair-mode () (autopair-mode 1))
+(dolist (mode autopair-modes) (add-hook (intern (concat (symbol-name mode) "-hook")) 'turn-on-autopair-mode))
 
 ;; disable autopair if paredit is turned on
 (defadvice paredit-mode (around disable-autopairs-around (arg))
